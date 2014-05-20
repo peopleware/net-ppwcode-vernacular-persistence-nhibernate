ae211430e0464c74aac93dacebdc88cfea25805f
Danny Van den Wouwer <danny_van_den_wouwer@peopleware.be>
2014-05-20 09:42:32 +0000

    Add ImmutableUserTypeBase.cs
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Persistence.nHibernate/dev/PPWCode.Vernacular.nHibernate.I@7602 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ src/PPWCode.Vernacular.nHibernate.I/Implementations/ImmutableUserTypeBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data;
+
+using NHibernate.SqlTypes;
+using NHibernate.UserTypes;
+
+namespace PPWCode.Vernacular.nHibernate.I.Implementations
+{
+    public abstract class ImmutableUserTypeBase : IUserType
+    {
+        #region Implementation of IUserType
+
+        public new bool Equals(object x, object y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return true;
+            }
+            if (x == null || y == null)
+            {
+                return false;
+            }
+
+            return x.Equals(y);
+        }
+
+        public int GetHashCode(object x)
+        {
+            if (x == null)
+            {
+                throw new ArgumentNullException("x");
+            }
+            return x.GetHashCode();
+        }
+
+        public abstract object NullSafeGet(IDataReader rs, string[] names, object owner);
+        public abstract void NullSafeSet(IDbCommand cmd, object value, int index);
+        public abstract object DeepCopy(object value);
+
+        public object Replace(object original, object target, object owner)
+        {
+            // since object is immutable, return original
+            return original;
+        }
+
+        public object Assemble(object cached, object owner)
+        {
+            //Used for cashing, as our object is immutable we can just return it as is
+            return cached;
+        }
+
+        public object Disassemble(object value)
+        {
+            //Used for caching, as our object is immutable we can just return it as is
+            return value;
+        }
+
+        public abstract SqlType[] SqlTypes { get; }
+        public abstract Type ReturnedType { get; }
+
+        public bool IsMutable
+        {
+            get { return false; }
+        }
+
+        #endregion
+    }
+}
