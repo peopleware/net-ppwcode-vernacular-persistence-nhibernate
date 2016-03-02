3b06e835306c1dafafd833cd0b9b1e9ee60b190f
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2016-03-02 19:02:58 +0100

    Upgrading to new fixed version of PPWCode.Vernacular.Persistence.II

--- src/Solution Items/SolutionInfo.cs
+++ src/Solution Items/SolutionInfo.cs
@@ -1,4 +1,4 @@
-// Copyright 2014 by PeopleWare n.v..
+// Copyright 2016 by PeopleWare n.v..
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Peopleware n.v.")]
 [assembly: AssemblyProduct("PPWCode Vernacular NHibernate")]
-[assembly: AssemblyCopyright("Copyright © PeopleWare n.v. 2014")]
+[assembly: AssemblyCopyright("Copyright © PeopleWare n.v. 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("2.0.0")]
