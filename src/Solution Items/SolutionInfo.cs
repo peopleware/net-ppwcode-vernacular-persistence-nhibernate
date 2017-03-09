0a53af13792571e3ad7a335475ac1c6bbcc5ceec
Danny Van den Wouwer <dvdwouwe@peopleware.be>
2017-03-09 15:34:37 +0100

    Bump to new version 2.0.5

--- src/Solution Items/SolutionInfo.cs
+++ src/Solution Items/SolutionInfo.cs
@@ -19,5 +19,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.4")]
-[assembly: AssemblyInformationalVersion("2.0.4")]
+[assembly: AssemblyFileVersion("2.0.5")]
+[assembly: AssemblyInformationalVersion("2.0.5")]
