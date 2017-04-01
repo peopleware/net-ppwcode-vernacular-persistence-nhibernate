783376594762fb939e499271d14acfb36239b5d9
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2017-04-02 01:08:43 +0200

    Adding test for query with mapping to DTO

---
+++ src/PPWCode.Vernacular.NHibernate.I.Tests/RepositoryWithDtoMapping/Models/CargoContainer.cs
@@ -0,0 +1,81 @@
+﻿// Copyright 2017 by PeopleWare n.v..
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Runtime.Serialization;
+
+using PPWCode.Vernacular.Persistence.II;
+
+namespace PPWCode.Vernacular.NHibernate.I.Tests.RepositoryWithDtoMapping.Models
+{
+    [Serializable, DataContract(IsReference = true)]
+    public class CargoContainer : PersistentObject<int>
+    {
+        [DataMember]
+        private string m_Code;
+        
+        [DataMember]
+        private int m_Load;
+
+        [DataMember]
+        private Ship m_Ship;
+
+        public CargoContainer()
+        {
+        }
+
+        public CargoContainer(int id) : base(id)
+        {
+        }
+
+        public virtual string Code
+        {
+            get { return m_Code; }
+            set { m_Code = value; }
+        }
+
+        public virtual int Load
+        {
+            get { return m_Load; }
+            set { m_Load = value; }
+        }
+
+        public virtual Ship Ship
+        {
+            get
+            {
+                return m_Ship;
+            }
+
+            set
+            {
+                if (m_Ship != value)
+                {
+                    Ship previousShip = m_Ship;
+                    m_Ship = value;
+
+                    if (previousShip != null)
+                    {
+                        previousShip.RemoveCargoContainer(this);
+                    }
+
+                    if (m_Ship != null)
+                    {
+                        m_Ship.AddCargoContainer(this);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/PPWCode.Vernacular.NHibernate.I.Tests/RepositoryWithDtoMapping/Repositories/ShipRepository.cs
@@ -0,0 +1,50 @@
+﻿// Copyright 2017 by PeopleWare n.v..
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections.Generic;
+
+using NHibernate;
+using NHibernate.Transform;
+
+using PPWCode.Vernacular.NHibernate.I.Tests.Repositories;
+using PPWCode.Vernacular.NHibernate.I.Tests.RepositoryWithDtoMapping.Models;
+
+namespace PPWCode.Vernacular.NHibernate.I.Tests.RepositoryWithDtoMapping.Repositories
+{
+    public class ShipRepository : TestRepository<Ship>, ITestRepository<Ship>
+    {
+        public ShipRepository(ISession session)
+            : base(session)
+        {
+        }
+
+        public IList<ContainerDto> FindContainersFromShipsMatchingCode(string code)
+        {
+            Ship ship = null;
+            CargoContainer cargoContainer = null;
+            ContainerDto dto = null;
+
+            return Find<ContainerDto>(
+                () => ship,
+                qry => qry
+                    .JoinAlias(() => ship.CargoContainers, () => cargoContainer)
+                    .WhereRestrictionOn(() => ship.Code).IsLike(code + "%")
+                    .SelectList(list => list
+                        .SelectGroup(() => ship.Code).WithAlias(() => dto.ShipCode)
+                        .SelectGroup(() => cargoContainer.Code).WithAlias(() => dto.ContainerCode)
+                        .SelectGroup(() => cargoContainer.Load).WithAlias(() => dto.Load))
+                    .TransformUsing(Transformers.AliasToBean<ContainerDto>()));
+        }
+    }
+}
