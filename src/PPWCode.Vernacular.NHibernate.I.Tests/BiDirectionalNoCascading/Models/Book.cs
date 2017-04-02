91d49dd37c1c807491194d771e32459e715c58c7
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2017-04-02 12:34:51 +0200

    Merge vs SaveOrUpdate for bi-directional relationship without cascading

---
+++ src/PPWCode.Vernacular.NHibernate.I.Tests/BiDirectionalNoCascading/Models/Book.cs
@@ -0,0 +1,86 @@
+﻿// Copyright 2017 by PeopleWare n.v..
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections.Generic;
+
+using PPWCode.Vernacular.Persistence.II;
+
+namespace PPWCode.Vernacular.NHibernate.I.Tests.BiDirectionalNoCascading.Models
+{
+    public class Book : PersistentObject<int>
+    {
+        private string m_Name;
+        private Author m_Author;
+        private readonly ISet<Keyword> m_Keywords = new HashSet<Keyword>();
+
+        public Book()
+        {
+        }
+
+        public Book(int id)
+            : base(id)
+        {
+        }
+
+        public virtual string Name
+        {
+            get { return m_Name; }
+            set { m_Name = value; }
+        }
+
+        public virtual Author Author
+        {
+            get { return m_Author; }
+            set
+            {
+                if (m_Author != value)
+                {
+                    Author previousAuthor = m_Author;
+                    m_Author = value;
+
+                    if (previousAuthor != null)
+                    {
+                        previousAuthor.RemoveBook(this);
+                    }
+
+                    if (m_Author != null)
+                    {
+                        m_Author.AddBook(this);
+                    }
+                }
+            }
+        }
+
+        public virtual ISet<Keyword> Keywords
+        {
+            get { return m_Keywords; }
+        }
+
+        public virtual void AddKeyword(Keyword keyword)
+        {
+            if (keyword != null && m_Keywords.Add(keyword))
+            {
+                keyword.AddBook(this);
+            }
+        }
+
+        public virtual void RemoveKeyword(Keyword keyword)
+        {
+            if (keyword != null && m_Keywords.Remove(keyword))
+            {
+                keyword.RemoveBook(this);
+            }
+        }
+    }
+}
---
+++ src/PPWCode.Vernacular.NHibernate.I.Tests/BiDirectionalNoCascading/Models/Keyword.cs
@@ -0,0 +1,62 @@
+﻿// Copyright 2017 by PeopleWare n.v..
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections.Generic;
+
+using PPWCode.Vernacular.Persistence.II;
+
+namespace PPWCode.Vernacular.NHibernate.I.Tests.BiDirectionalNoCascading.Models
+{
+    public class Keyword : PersistentObject<int>
+    {
+        private string m_Name;
+        private readonly ISet<Book> m_Books = new HashSet<Book>();
+
+        public Keyword()
+        {
+        }
+
+        public Keyword(int id)
+            : base(id)
+        {
+        }
+
+        public virtual string Name
+        {
+            get { return m_Name; }
+            set { m_Name = value; }
+        }
+
+        public virtual ISet<Book> Books
+        {
+            get { return m_Books; }
+        }
+
+        public virtual void AddBook(Book book)
+        {
+            if (book != null && m_Books.Add(book))
+            {
+                book.AddKeyword(this);
+            }
+        }
+
+        public virtual void RemoveBook(Book book)
+        {
+            if (book != null && m_Books.Remove(book))
+            {
+                book.RemoveKeyword(this);
+            }
+        }
+    }
+}
