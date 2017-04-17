cdff42fa7b225b50e6ad7de40b1500db495c4c3a
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2017-04-17 19:59:39 +0200

    Added test for testing FindPagedInternal

--- src/PPWCode.Vernacular.NHibernate.I.Tests/RepositoryWithDtoMapping/Repositories/ShipRepository.cs
+++ src/PPWCode.Vernacular.NHibernate.I.Tests/RepositoryWithDtoMapping/Repositories/ShipRepository.cs
@@ -19,15 +19,47 @@ using NHibernate.Transform;
 
 using PPWCode.Vernacular.NHibernate.I.Tests.Repositories;
 using PPWCode.Vernacular.NHibernate.I.Tests.RepositoryWithDtoMapping.Models;
+using PPWCode.Vernacular.Persistence.II;
 
 namespace PPWCode.Vernacular.NHibernate.I.Tests.RepositoryWithDtoMapping.Repositories
 {
-    public class ShipRepository : TestRepository<Ship>,
-                                  ITestRepository<Ship>
+    public class ShipRepository : TestRepository<Ship>
     {
         public ShipRepository(ISession session)
             : base(session)
         {
+        }
+
+        private PagedList<ContainerDto> InternalFindContainersFromShipsMatchingCodePaged(int pageIndex, int pageSize, string code)
+        {
+            return FindPagedInternal<ContainerDto>(
+                pageIndex,
+                pageSize,
+                () =>
+                {
+                    CargoContainer cargoContainer = null;
+                    ContainerDto dto = null;
+
+                    return
+                        CreateQueryOver()
+                            .JoinAlias(ship => ship.CargoContainers, () => cargoContainer)
+                            .WhereRestrictionOn(ship => ship.Code)
+                            .IsLike(code + "%")
+                            .OrderBy(ship => ship.Code).Asc
+                            .SelectList(list => list
+                                            .SelectGroup(ship => ship.Code)
+                                            .WithAlias(() => dto.ShipCode)
+                                            .SelectGroup(ship => cargoContainer.Code)
+                                            .WithAlias(() => dto.ContainerCode)
+                                            .SelectGroup(ship => cargoContainer.Load)
+                                            .WithAlias(() => dto.Load))
+                            .TransformUsing(Transformers.AliasToBean<ContainerDto>());
+                });
+        }
+
+        public PagedList<ContainerDto> FindContainersFromShipsMatchingCodePaged(int pageIndex, int pageSize, string code)
+        {
+            return Execute("FindContainersFromShipsMatchingCodePaged", () => InternalFindContainersFromShipsMatchingCodePaged(pageIndex, pageSize, code));
         }
 
         private IList<ContainerDto> InternalFindContainersFromShipsMatchingCode(string code)
