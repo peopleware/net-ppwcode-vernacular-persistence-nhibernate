54d02fcb078910fe2a187077f2c627883a935fed
Danny Van den Wouwer <dvdwouwe@peopleware.be>
2017-12-31 12:30:04 +0100

    Improve PpwAuxiliaryDatabaseObject further (in progress)

--- src/PPWCode.Vernacular.NHibernate.I/Utilities/PpwAuxiliaryDatabaseObject.cs
+++ src/PPWCode.Vernacular.NHibernate.I/Utilities/PpwAuxiliaryDatabaseObject.cs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
+using System.Linq;
+
 using NHibernate.Cfg;
 using NHibernate.Mapping;
 
@@ -36,6 +39,46 @@ namespace PPWCode.Vernacular.NHibernate.I.Utilities
         public void SetConfiguration(Configuration configuration)
         {
             m_Configuration = configuration;
+        }
+
+        protected virtual PersistentClass GetPersistentClassFor(Type type)
+        {
+            return
+                Configuration
+                    .ClassMappings
+                    .Single(m => m.MappedClass == type);
+        }
+
+        protected virtual string GetTableNameFor(Type type)
+        {
+            return GetPersistentClassFor(type)?.Table.Name;
+        }
+
+        protected virtual string GetDiscriminatorColumnNameFor(Type type)
+        {
+            Column column =
+                GetPersistentClassFor(type)
+                    ?.Discriminator
+                    ?.ColumnIterator
+                    .OfType<Column>()
+                    .FirstOrDefault();
+            return column?.Name;
+        }
+
+        protected virtual string[] GetDiscriminatorValuesFor(Type type)
+        {
+            return
+                Configuration
+                    .ClassMappings
+                    .SelectMany(classMapping => classMapping.DirectSubclasses, (classMapping, subclass) => new { classMapping, subclass })
+                    .Where(t => t.classMapping.MappedClass == type && !t.subclass.MappedClass.IsAbstract)
+                    .Select(t => t.subclass.DiscriminatorValue)
+                    .Union(
+                        Configuration
+                            .ClassMappings
+                            .Where(m => m.MappedClass == type && !m.MappedClass.IsAbstract)
+                            .Select(m => m.DiscriminatorValue))
+                    .ToArray();
         }
     }
 }
