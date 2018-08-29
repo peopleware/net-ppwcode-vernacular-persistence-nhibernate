f31bfb1be4c0b71208738dc75feb03b78afe3f5e
Danny Van den Wouwer <dvdwouwe@peopleware.be>
2018-08-29 22:33:35 +0200

    Add explicit test to create database schema scripts

--- src/II.Tests/ConventionMappingTests.cs
+++ src/II.Tests/ConventionMappingTests.cs
@@ -11,16 +11,49 @@
 
 using System;
 
+using NHibernate.Cfg;
 using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping;
 using NHibernate.Mapping.ByCode;
+using NHibernate.Tool.hbm2ddl;
 
 using NUnit.Framework;
+
+using PPWCode.Vernacular.NHibernate.II.SqlServer;
+using PPWCode.Vernacular.NHibernate.II.Tests.Models;
 
 namespace PPWCode.Vernacular.NHibernate.II.Tests
 {
     [TestFixture]
     public class ConventionMappingTests
     {
+        [Test]
+        [Explicit]
+        public void DatabaseScript()
+        {
+            Configuration configuration =
+                new Configuration()
+                    .DataBaseIntegration(db => db.Dialect<MsSqlDialect>())
+                    .Configure();
+            IPpwHbmMapping mapper = new TestsSimpleModelMapper(new TestsMappingAssemblies());
+            HbmMapping hbmMapping = mapper.HbmMapping;
+            configuration.AddMapping(hbmMapping);
+            IAuxiliaryDatabaseObject[] auxiliaryDatabaseObjects =
+            {
+                new TestHighLowPerTableAuxiliaryDatabaseObject(mapper),
+                new UniqueConstraintsForExtendedCompany(mapper)
+            };
+            foreach (IAuxiliaryDatabaseObject auxiliaryDatabaseObject in auxiliaryDatabaseObjects)
+            {
+                IPpwAuxiliaryDatabaseObject ppwAuxiliaryDatabaseObject = auxiliaryDatabaseObject as IPpwAuxiliaryDatabaseObject;
+                ppwAuxiliaryDatabaseObject?.SetConfiguration(configuration);
+                configuration.AddAuxiliaryDatabaseObject(auxiliaryDatabaseObject);
+            }
+
+            SchemaExport schemaExport = new SchemaExport(configuration);
+            schemaExport.Create(true, false);
+        }
+
         [Test]
         [Explicit]
         public void XmlMapping()
