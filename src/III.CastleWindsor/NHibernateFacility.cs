6f806ab34c3a292fb1a59beaaeecfbd29adf8023
Danny Van den Wouwer <dvdwouwe@peopleware.be>
2020-05-24 15:21:49 +0200

    Consistent LifeStyle specification

--- src/III.CastleWindsor/NHibernateFacility.cs
+++ src/III.CastleWindsor/NHibernateFacility.cs
@@ -1,4 +1,4 @@
-// Copyright 2018 by PeopleWare n.v..
+﻿// Copyright 2018 by PeopleWare n.v..
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -381,7 +381,7 @@ namespace PPWCode.Vernacular.NHibernate.III.CastleWindsor
                         Component
                             .For<IQueryOverCustomExpressions>()
                             .ImplementedBy(_queryOverCustomExpressions)
-                            .LifestyleSingleton());
+                            .LifeStyle.Singleton);
                 IQueryOverCustomExpressions queryOverCustomExpressions = Kernel.Resolve<IQueryOverCustomExpressions>();
                 queryOverCustomExpressions.Initialize();
             }
